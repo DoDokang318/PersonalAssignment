c60bef73e1fa388594b10476fbe786c0a657a91e
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-08-18 14:35:11 +0900

    first222
    sad

--- PersonalAssignment/Program.cs
+++ PersonalAssignment/Program.cs
@@ -10,6 +10,29 @@ namespace PersonalAssignment
     {
         static void Main(string[] args)
         {
+            
+
+            /*스파르타 마을에 오신 여러분 환영합니다.
+            이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.
+
+                1. 상태 보기
+                2. 인벤토리
+
+            원하시는 행동을 입력해주세요.
+            >>*/
+
+             firstDisplay();
+
+
+
         }
+
+        static void firstDisplay()
+        {
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다");
+            Console.WriteLine(" 1. 상태 보기\n 2. 인벤토리");
+
+        }
+
     }
 }
