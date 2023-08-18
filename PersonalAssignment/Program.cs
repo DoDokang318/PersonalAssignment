9492aacfb8ba57be0ad7ca9c1f53e2a54a4d680d
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-08-18 21:42:52 +0900

    인밴토리
    인밴토리

--- PersonalAssignment/Program.cs
+++ PersonalAssignment/Program.cs
@@ -10,11 +10,14 @@ namespace PersonalAssignment
     {
 
         private static player Player;
-       
+      
+
+        private static equipment Sword;
+        private static equipment Shield;
+
         static void Main(string[] args)
         {
             playerData();
-          
             firstDisplay();
 
         }
@@ -27,21 +30,30 @@ namespace PersonalAssignment
             
         }
 
+        static void EquipmentData()
+        {
+            Sword = new equipment("칼", 8, "짱쌘칼");
+            Shield = new equipment("방패 ", 5, "짱쌘방패");
 
-        static void firstDisplay()
+
+        }
+
+
+            static void firstDisplay()
         {
             Console.WriteLine($"스파르타 마을에 오신 {Player.Name }님 환영합니다\n.이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다");
             Console.WriteLine(" 1. 상태 보기\n 2. 인벤토리");
 
-
+           
             int input = CheckInput(1, 2);
             switch (input)
             {
                 case 1:
                     PlayerInfo();
-                  
                     break;
                 case 2:
+                    Inven();
+               
                     break;
             }
 
@@ -70,11 +82,45 @@ namespace PersonalAssignment
             {
                 case 0:
                     firstDisplay();
-
-                    break;
+                break;
 
             }
             
+        }
+
+
+        ///<summary>
+        ///플레이어의 인벤토리에 관련된  메서드 이다 
+       ///</summary>
+        public static void Inven()
+        {
+            EquipmentData();
+
+
+            Console.WriteLine("인벤토리\n");
+          
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다\n\n");
+          
+            Console.WriteLine("[아이템 목록]\n");
+
+            Console.WriteLine(Sword.Eq + "      l   " + Sword.Stat + "   l   " + Sword.EqExplain+"\n");
+            Console.WriteLine(Shield.Eq + "   l   " + Shield.Stat + "   l   " + Shield.EqExplain);
+
+
+            Console.WriteLine("\n\n\n\n\n");
+
+
+
+            Console.WriteLine("0 :나가기");
+            int input = CheckInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    firstDisplay();
+                    break;
+
+            }
+
         }
 
 
@@ -89,11 +135,13 @@ namespace PersonalAssignment
             {
                 string input = Console.ReadLine();
 
-                Console.Clear();
+                
                 parseSuccess = int.TryParse(input, out DispStat);
 
                 if (parseSuccess && DispStat >= min && DispStat <= max)
                 {
+
+                    Console.Clear();
                     return DispStat;
                   
 
@@ -104,7 +152,21 @@ namespace PersonalAssignment
         }
     }
 
+    public class equipment
+    {
 
+        public string Eq { get; }
+        public int Stat { get; }
+        public string EqExplain { get; }
+        public equipment(string eq, int stat, string eqExplain )
+        {
+            Eq = eq;
+
+            Stat = stat;
+
+            EqExplain = eqExplain;
+        }
+    }
     public class player
     {
       
